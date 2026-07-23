dca56031f15ec739f0e5d0ce468cd288d5f07ae1
Ali-king-cloud <alibraheem064@gmail.com>
2026-07-23 23:05:26 +0500

    Details.py

---
+++ monitor mode/DETAILS.py
@@ -0,0 +1,24 @@
+import subprocess
+
+dict = {
+    "all_details": ["ipconfig","all"],
+    "only_mac": ["getmac /v"]
+}
+my_input = input("ENter  your number \n 1 for the all details related to mac \n 2 for the only mac")
+try:
+    if my_input == "1":
+        details = subprocess.run([dict["all_details"]],
+                                 capture_output=True,
+                                 text=True,
+                                 check=True)
+        print(details.stdout)
+    elif my_input == "2":
+        details = subprocess.run([dict["only_mac"]],
+                                 capture_output=True,
+                                 text=True,
+                                 check=True)
+        print(details.stdout)
+    else:
+        print("be resonable bro")
+except Exception as e:
+    print(f"bhai koi serious error agya hai : {e}")
---
+++ monitor mode/arp_scan.py
@@ -0,0 +1,5 @@
+from scapy.all import ARP, Ether, srp
+
+class ARPScanner:
+    def __init__(self):
+        pass
